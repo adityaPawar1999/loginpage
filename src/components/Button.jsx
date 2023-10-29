33da77f660640b392be3a40cc5b42ea45bf54414
aditya pawar <113058497+adityaPawar1999@users.noreply.github.com>
2023-10-29 18:10:27 +0530

    Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,7 +8,7 @@ const StyledButton = styled.button`
   background: linear-gradient(to right, #14163c 0%, #03217b 79%);
   text-transform: uppercase;
   letter-spacing: 0.2rem;
-  width: 65%;
+  width: 80%;
   height: 3rem;
   border: none;
   color: white;
